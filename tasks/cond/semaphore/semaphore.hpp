d3fe76e74264cf884a738a91d5b1c4872e1c9381
tnsoftbear <avi-team@inbox.lv>
2023-10-31 03:53:30 +0300

    Solve cond/semaphore

--- tasks/cond/semaphore/semaphore.hpp
+++ tasks/cond/semaphore/semaphore.hpp
@@ -1,24 +1,65 @@
 #pragma once
 
+#include <condition_variable>
+#include <mutex>
 #include <twist/ed/stdlike/mutex.hpp>
 #include <twist/ed/stdlike/condition_variable.hpp>
 
 #include <cstdlib>
 
+#include <twist/ed/stdlike/thread.hpp>
+#include <sstream>
+
+using twist::ed::stdlike::condition_variable;
+using twist::ed::stdlike::mutex;
+
+//const bool kShouldPrint = true;
+const bool kShouldPrint = false;
+
 class Semaphore {
  public:
-  explicit Semaphore(size_t /*tokens*/) {
-    // Not implemented
+  explicit Semaphore(size_t tokens) {
+    tokens_ = tokens;
+  }
+
+  ~Semaphore() {
+    wait_cv_.notify_all();
   }
 
   void Acquire() {
-    // Not implemented
+    std::unique_lock<mutex> lock(*wait_mtx_);
+    wait_cv_.wait(lock, [this]{
+      return tokens_ != 0;
+    });
+    --tokens_;
   }
 
   void Release() {
-    // Not implemented
+    std::lock_guard<mutex> lock(*wait_mtx_);
+    ++tokens_;
+    if (tokens_ == 1) {
+      wait_cv_.notify_all();
+    }
   }
 
  private:
-  // Tokens
+  size_t tokens_;
+  std::unique_ptr<mutex> wait_mtx_ = std::make_unique<mutex>();
+  condition_variable wait_cv_;
+
+ private:
+  void Ll(const char* format, ...) {
+    if (!kShouldPrint) {
+      return;
+    }
+
+    char buf [250];
+    std::ostringstream pid;
+    pid << "[" << twist::ed::stdlike::this_thread::get_id() << "]";
+    sprintf(buf, "Semaphore: %s %s\n", pid.str().c_str(), format);
+    va_list args;
+    va_start(args, format);
+    vprintf(buf, args);
+    va_end(args);
+  }
 };
